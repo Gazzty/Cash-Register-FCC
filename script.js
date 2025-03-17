0eb866a33adc7e2b1b55b92035059b4df81049e7
Gazzty <tomasgustavo99@gmail.com>
2025-03-17 16:26:46 -0300

    Added change values to the HTML

--- script.js
+++ script.js
@@ -12,4 +12,22 @@ let cid = [
 ];
 
 const priceHTML = document.getElementById("price")
+const penny = document.getElementById("pennies")
+const nickel = document.getElementById("nickels")
+const dime = document.getElementById("dimes")
+const quarter = document.getElementById("quarters")
+const one = document.getElementById("ones")
+const five = document.getElementById("fives")
+const ten = document.getElementById("tens")
+const hundred = document.getElementById("hundreds")
+
+// Add text in HTML
 priceHTML.innerText = `Total: $${price}`
+penny.innerText += `${cid[0][1]}`
+nickel.innerText += `${cid[1][1]}`
+dime.innerText += `${cid[2][1]}`
+quarter.innerText += `${cid[3][1]}`
+one.innerText += `${cid[4][1]}`
+five.innerText += `${cid[5][1]}`
+ten.innerText += `${cid[6][1]}`
+hundred.innerText += `${cid[7][1]}`
